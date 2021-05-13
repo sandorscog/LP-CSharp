9b1a82e694e5f40ddaacf839fc82086e22fde184
Sandor <49007952+sandorscog@users.noreply.github.com>
2021-05-13 07:30:23 -0300

    Add files via upload

---
+++ ArvoreDeExpressao.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+namespace DJOUZE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParameterExpression value = Expression.Parameter(typeof(int), "value");
+            ParameterExpression result = Expression.Parameter(typeof(bool), "result");
+            
+            LabelTarget returnTarget = Expression.Label(typeof(bool));
+            LabelExpression returnLabel = Expression.Label(returnTarget, Expression.Constant(true));
+            GotoExpression returnExpression = Expression.Return(returnTarget,
+             result, typeof(bool));
+
+            BlockExpression block = Expression.Block(
+                new[] { result },
+                Expression.Assign(result, Expression.Constant(true)),
+                Expression.IfThenElse(
+                    Expression.Equal(Expression.Modulo(value, Expression.Constant(2)), Expression.Constant(0)),
+                    Expression.Assign(result,Expression.Constant(true)),
+                    Expression.Assign(result, Expression.Constant(false))
+                ),
+                returnExpression,returnLabel
+            );
+            int num = int.Parse(Console.ReadLine());
+            bool isPar = Expression.Lambda<Func<int, bool>>(block, value).Compile()(num);
+            Console.WriteLine(isPar);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ InvertLista.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+namespace DJOUZE
+{
+    class Program {
+        static void invert(List<int> lista)
+        {
+            int aux = 0;
+            for(int i = 0; i < lista.Count / 2; i++)
+            {
+                aux = lista[i];
+                lista[i] = lista[lista.Count - 1-i];
+                lista[lista.Count - 1 - i] = aux;
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int>();
+            Console.WriteLine("Quantos elementos?");
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                lista.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine("Lista antes da inversao:");
+            for(int i = 0; i < lista.Count; i++)
+            {
+                Console.Write(lista[i]+" ");
+            }
+            Console.WriteLine("");
+            invert(lista);
+            Console.WriteLine("Lista apos a inversao:");
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.Write(lista[i] + " ");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
